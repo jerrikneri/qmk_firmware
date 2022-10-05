d7f617858ed783e285846a1e7fe0275dde9db28f
Jerrik <jerrik.neri@fashionphile.com>
2022-10-04 20:55:20 -0700

    preonic mapping

--- keyboards/preonic/keymaps/jerrik/keymap.c
+++ keyboards/preonic/keymaps/jerrik/keymap.c
@@ -26,8 +26,8 @@ enum preonic_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
-  _FUNCTION,
-  _FUNCTION2,
+  _FN,
+  _FN2,
   _ADJUST
 };
 
@@ -36,7 +36,7 @@ enum preonic_keycodes {
   DEV,
   MOUSE,
   NAV,
-  FUNCTION,
+  FN,
   COLEMAK,
   DVORAK,
   LOWER,
@@ -47,8 +47,8 @@ enum preonic_keycodes {
 #define DEV MO(_DEV)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define FUNCTION TG(_FUNCTION)
-#define FUNCTION2 TG(_FUNCTION2)
+#define FN TG(_FN)
+#define FN2 TG(_FN2)
 
 #define KC_RCPP LGUI(LSFT(KC_5))        // Record portion of screen
 #define KC_CAPP LGUI(LSFT(KC_4))        // Capture portion of screen
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R , KC_T  , KC_Y   , KC_U , KC_I   , KC_O   , KC_P   , KC_MINS  ,
   KC_X0   , KC_A   , KC_S   , KC_D   , KC_F , KC_G  , KC_H   , KC_J , KC_K   , KC_L   , KC_SCLN, KC_QUOT  ,
   KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V , KC_B  , KC_N   , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_X4    ,
-  FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_XSP, KC_XBS , RAISE, DEV    , _______, _______, FUNCTION2
+  FN      , KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_XBS , RAISE, DEV    , KC_RALT, KC_RCTL, FN2
 ),
 
 [_DEV] = LAYOUT_preonic_grid(
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_MOUSE] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_WH_U, KC_MS_U, KC_WH_D, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______,
@@ -210,7 +210,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-[_FUNCTION] = LAYOUT_preonic_grid(
+[_FN] = LAYOUT_preonic_grid(
   KC_F11  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12 ,
   KC_F11  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12 ,
   KC_CAPS , _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
@@ -218,7 +218,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______ , _______, _______, _______, _______, KC_INS , KC_DEL , _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
-[_FUNCTION2] = LAYOUT_preonic_grid(
+[_FN2] = LAYOUT_preonic_grid(
   KC_F11 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12   ,
   KC_F11 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12   ,
   KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______  ,
@@ -257,9 +257,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
-        case FUNCTION:
+        case FN:
           if (record->event.pressed) {
-            set_single_persistent_default_layer(_FUNCTION);
+            set_single_persistent_default_layer(_FN);
           }
           return false;
           break;
@@ -349,33 +349,6 @@ uint8_t last_muse_note = 0;
 uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
-    } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    }
-  }
-    return true;
-}
 
 bool dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
